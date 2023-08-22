17d048e28991a3258f9ec348c963680ffae1d8d3
Kamilm55 <kamilmmmdov2905@gmail.com>
2023-08-22 21:37:37 +0400

    servlet lessons

---
+++ src/main/java/com/Example/AddServlet.java
@@ -0,0 +1,39 @@
+package com.Example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.ProcessBuilder.Redirect;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class AddServlet extends HttpServlet
+{
+
+	public void doPost(HttpServletRequest req , HttpServletResponse res) throws IOException, ServletException
+	{
+		int num1 = Integer.parseInt(req.getParameter("num1"));
+		int num2 = Integer.parseInt(req.getParameter("num2"));
+		
+		int result = num1 + num2;
+
+//		HttpSession session = req.getSession();
+//		session.setAttribute("result",result );
+		Cookie cookie = new Cookie("result",Integer.toString(result));
+		res.addCookie(cookie);
+		
+		res.sendRedirect("SqServlet");
+		
+//		res.sendRedirect("SqServlet?result=" + result);
+
+//		req.setAttribute("result",result );
+//		RequestDispatcher rDispatcher = req.getRequestDispatcher("SqServlet");
+//		rDispatcher.forward(req, res);
+		
+	}
+}
---
+++ src/main/java/com/Example/SqServlet.java
@@ -0,0 +1,52 @@
+package com.Example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class SqServlet extends HttpServlet {
+//	Cookies and sessions
+//	are used by websites to store users' data across different pages of the site. The key difference between sessions and cookies is that sessions are saved on the server side while cookies are saved on the client side. Cookies are small text files used to store user information on the user's computer.
+	
+	public void doGet(HttpServletRequest req , HttpServletResponse res) throws IOException
+	{
+//		int result = (int) req.getAttribute("result");
+//		int result = Integer.parseInt(req.getParameter("result"));
+//		HttpSession session = req.getSession();
+//		int result = (int) session.getAttribute("result");
+		
+		Cookie cookies[] = req.getCookies();
+		
+		int result = 0;
+		for(Cookie c : cookies ) {
+			if (c.getName().equals("result")) {
+			result = Integer.parseInt(c.getValue());
+		}}
+		
+		int squareResult = result * result;
+		
+		ServletContext ctx = getServletContext();
+		String name = ctx.getInitParameter("name");//These context values are available from any servlet
+		String phone = ctx.getInitParameter("phone");
+		
+		ServletConfig config = getServletConfig();
+		String nameConfig = config.getInitParameter("nameConfig");//Config is sepecific to a particular servlet
+		
+		PrintWriter out = res.getWriter();
+		out.println("<html><body style=\"background: green;\">");
+		out.println("Output is: " + squareResult + "<br/>");
+		out.println("Name is:" + name + "<br/>");
+		out.println("Phone is:" + phone + "<br/><br/><br/>");
+		
+		out.println("Config name is:" + nameConfig + "<br/>");
+		out.println("</body></html>");
+	}
+}
